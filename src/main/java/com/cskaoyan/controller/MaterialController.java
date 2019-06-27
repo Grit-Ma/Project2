1c6ab7c526382af8f6ed92ed689646d6e4cbe88f
cdy17test <cdy17test@163.com>
2019-06-27 21:16:31 +0800

    做了一点

--- src/main/java/com/cskaoyan/controller/MaterialController.java
+++ src/main/java/com/cskaoyan/controller/MaterialController.java
@@ -3,12 +3,13 @@ package com.cskaoyan.controller;
 import com.cskaoyan.bean.Material;
 import com.cskaoyan.service.MaterialService;
 import com.cskaoyan.vo.PageVo;
+import com.cskaoyan.vo.ResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.List;
+import java.util.Arrays;
 
 @Controller
 public class MaterialController {
@@ -17,14 +18,45 @@ public class MaterialController {
     MaterialService materialService;
 
     @RequestMapping("material/find")
-    public String getFind(){
+    public String find(){
         return "/WEB-INF/jsp/material_list.jsp";
     }
 
     @RequestMapping("material/list")
     @ResponseBody
-    public PageVo getList(int page, int rows){
+    public PageVo list(int page, int rows){
         PageVo pageVo = materialService.getPage(page, rows);
         return pageVo;
+    }
+
+    @RequestMapping("material/edit_judge")
+    public String edit(){
+        return "/WEB-INF/jsp/material_edit.jsp";
+    }
+
+    @RequestMapping("material/update_all")
+    @ResponseBody
+    public ResponseVo update(Material material){
+        ResponseVo responseVo = materialService.updateMaterial(material);
+        return responseVo;
+    }
+
+    @RequestMapping("material/add")
+    public String add(){
+        return "/WEB-INF/jsp/material_add.jsp";
+    }
+
+    @RequestMapping("material/insert")
+    @ResponseBody
+    public ResponseVo insert(Material material){
+        ResponseVo responseVo = materialService.insertMaterial(material);
+        return responseVo;
+    }
+
+    @RequestMapping("material/delete")
+    @ResponseBody
+    public ResponseVo delete_batch(String[] ids){
+        ResponseVo responseVo = materialService.batchDeleteMaterialByIds(Arrays.asList(ids));
+        return responseVo;
     }
 }
--- src/main/java/com/cskaoyan/mapper/MaterialMapper.java
+++ src/main/java/com/cskaoyan/mapper/MaterialMapper.java
@@ -1,13 +1,30 @@
 package com.cskaoyan.mapper;
 
 import com.cskaoyan.bean.Material;
-
+import com.cskaoyan.bean.MaterialExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface MaterialMapper {
+    long countByExample(MaterialExample example);
 
-    int getAllCount();
+    int deleteByExample(MaterialExample example);
 
-    List<Material> getMaterial();
+    int deleteByPrimaryKey(String materialId);
+
+    int insert(Material record);
+
+    int insertSelective(Material record);
+
+    List<Material> selectByExample(MaterialExample example);
+
+    Material selectByPrimaryKey(String materialId);
+
+    int updateByExampleSelective(@Param("record") Material record, @Param("example") MaterialExample example);
+
+    int updateByExample(@Param("record") Material record, @Param("example") MaterialExample example);
+
+    int updateByPrimaryKeySelective(Material record);
+
+    int updateByPrimaryKey(Material record);
 }
