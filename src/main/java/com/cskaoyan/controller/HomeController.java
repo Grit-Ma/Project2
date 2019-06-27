e6b3960682a05546c7de4d0e03cd6c0159a7409f
cdy17test <cdy17test@163.com>
2019-06-27 15:36:22 +0800

    new push

--- src/main/java/com/cskaoyan/controller/HomeController.java
+++ src/main/java/com/cskaoyan/controller/HomeController.java
@@ -3,16 +3,6 @@ package com.cskaoyan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-<<<<<<< HEAD
-@Controller
-public class HomeController {
-    @RequestMapping("home")
-    public String home() {
-        return "/WEB-INF/jsp/home.jsp";
-    }
-
-=======
-
 @Controller
 public class HomeController {
     @RequestMapping("home")
@@ -24,5 +14,4 @@ public class HomeController {
     public String login(){
         return "/WEB-INF/jsp/login.jsp";
     }
->>>>>>> 98ca399295314fb6c92e4e5168e3e86ec5cef183
 }
