a7a2dc16907e2c26e0d9ae9ce885bae8ab008b89
cdy17test <cdy17test@163.com>
2019-06-28 12:02:10 +0800

    new

--- src/main/java/com/cskaoyan/controller/material/MaterialController.java
+++ src/main/java/com/cskaoyan/controller/material/MaterialController.java
@@ -1,7 +1,7 @@
-package com.cskaoyan.controller;
+package com.cskaoyan.controller.material;
 
 import com.cskaoyan.bean.Material;
-import com.cskaoyan.service.MaterialService;
+import com.cskaoyan.service.material.MaterialService;
 import com.cskaoyan.vo.PageVo;
 import com.cskaoyan.vo.ResponseVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,7 +50,14 @@ public class MaterialController {
     @RequestMapping("material/search_material_by_materialId")
     @ResponseBody
     public PageVo search_material_by_materialId(String searchValue, int page, int rows){
-        PageVo pageVo = materialService.search_material_by_materialId(searchValue, page, rows);
+        PageVo pageVo = materialService.searchMaterialByMaterialId(searchValue, page, rows);
+        return pageVo;
+    }
+
+    @RequestMapping("material/search_material_by_materialType")
+    @ResponseBody
+    public PageVo search_material_by_materialType(String searchValue, int page, int rows){
+        PageVo pageVo = materialService.searchMaterialByMaterialType(searchValue, page, rows);
         return pageVo;
     }
 
@@ -79,7 +86,14 @@ public class MaterialController {
         return responseVo;
     }
 
-    @RequestMapping("material/delete")
+    @RequestMapping("material/delete_judge")
+    @ResponseBody
+    public String delete_judge(){
+        return null;
+    }
+
+
+    @RequestMapping("material/delete_batch")
     @ResponseBody
     public ResponseVo delete_batch(String[] ids){
         ResponseVo responseVo = materialService.batchDeleteMaterialByIds(Arrays.asList(ids));
---
+++ src/main/java/com/cskaoyan/service/material/impl/MaterialReceiveServiceImpl.java
@@ -0,0 +1,128 @@
+package com.cskaoyan.service.material.impl;
+
+import com.cskaoyan.bean.Material_receive;
+import com.cskaoyan.bean.Material_receiveExample;
+import com.cskaoyan.mapper.Material_receiveMapper;
+import com.cskaoyan.service.material.MaterialReceiveService;
+import com.cskaoyan.tool.PageTool;
+import com.cskaoyan.vo.PageVo;
+import com.cskaoyan.vo.ResponseVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MaterialReceiveServiceImpl implements MaterialReceiveService {
+
+    @Autowired
+    Material_receiveMapper material_receiveMapper;
+
+    @Override
+    public List<Material_receive> getMaterialReceive() {
+        List<Material_receive> material_receiveList = material_receiveMapper.selectByExample(new Material_receiveExample());
+        return material_receiveList;
+    }
+
+    @Override
+    public List<Material_receive> getMaterialReceiveByReceiveId(String searchValue) {
+        Material_receiveExample material_receiveExample = new Material_receiveExample();
+        Material_receiveExample.Criteria criteria = material_receiveExample.createCriteria();
+        criteria.andReceiveIdLike("%" + searchValue + "%");
+        List<Material_receive> material_receiveList = material_receiveMapper.selectByExample(material_receiveExample);
+        return material_receiveList;
+    }
+
+    @Override
+    public List<Material_receive> getMaterialReceiveByMaterialId(String searchValue) {
+        Material_receiveExample material_receiveExample = new Material_receiveExample();
+        Material_receiveExample.Criteria criteria = material_receiveExample.createCriteria();
+        criteria.andMaterialIdLike("%" + searchValue + "%");
+        List<Material_receive> material_receiveList = material_receiveMapper.selectByExample(material_receiveExample);
+        return material_receiveList;
+    }
+
+
+    @Override
+    public PageVo getPage(int page, int rows) {
+        List<Material_receive> materials = getMaterialReceive();
+        PageVo pages = PageTool.getPageVo(materials, page, rows);
+        return pages;
+    }
+
+    @Override
+    public ResponseVo updateMaterialReceive(Material_receive material_receive) {
+        ResponseVo responseVo = new ResponseVo();
+        try{
+            material_receiveMapper.updateByPrimaryKey(material_receive);
+            responseVo.setMsg("修改成功");
+            responseVo.setStatus(200);
+        }catch (Exception e){
+            responseVo.setMsg("修改失败:"+e.getMessage());
+            responseVo.setStatus(500);
+        }
+        return responseVo;
+    }
+
+    @Override
+    public ResponseVo insertMaterialReceive(Material_receive material_receive) {
+        ResponseVo responseVo = new ResponseVo();
+        try{
+            material_receiveMapper.insert(material_receive);
+            responseVo.setMsg("新增成功");
+            responseVo.setStatus(200);
+        }catch (Exception e){
+            responseVo.setMsg("新增失败:"+e.getMessage());
+            responseVo.setStatus(500);
+        }
+        return responseVo;
+    }
+
+    @Override
+    public ResponseVo batchDeleteMaterialReceiveByIds(List<String> ids) {
+        ResponseVo responseVo = new ResponseVo();
+        Material_receiveExample example = new Material_receiveExample();
+        Material_receiveExample.Criteria criteria = example.createCriteria();
+        criteria.andMaterialIdIn(ids);
+        try{
+            material_receiveMapper.deleteByExample(example);
+            responseVo.setMsg("删除成功");
+            responseVo.setStatus(200);
+        }catch (Exception e){
+            responseVo.setMsg("删除失败");
+            responseVo.setStatus(500);
+        }
+        return responseVo;
+    }
+
+    @Override
+    public ResponseVo updateMaterialReceiveNote(Material_receive material_receive) {
+        Material_receive material_receive1 = material_receiveMapper.selectByPrimaryKey(material_receive.getMaterialId());
+        material_receive1.setNote(material_receive.getNote());
+        ResponseVo responseVo = new ResponseVo();
+        try{
+            material_receiveMapper.updateByPrimaryKey(material_receive1);
+            responseVo.setMsg("修改成功");
+            responseVo.setStatus(200);
+        }catch (Exception e){
+            responseVo.setMsg("修改失败:"+e.getMessage());
+            responseVo.setStatus(500);
+        }
+        return responseVo;
+    }
+
+    @Override
+    public PageVo searchMaterialReceiveByReceiveId(String searchValue, int page, int rows) {
+        List<Material_receive> materials = getMaterialReceiveByReceiveId(searchValue);
+        PageVo pages = PageTool.getPageVo(materials, page, rows);
+        return pages;
+    }
+
+    @Override
+    public PageVo searchMaterialReceiveByMaterialId(String searchValue, int page, int rows) {
+        List<Material_receive> materials = getMaterialReceiveByMaterialId(searchValue);
+        PageVo pages = PageTool.getPageVo(materials, page, rows);
+        return pages;
+    }
+
+}
